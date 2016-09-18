515abe2f5fb2be565de736062d6346a0e767875b
Sameer Singh <sameer.100.singh@gmail.com>
2016-09-19 00:45:26 +0530

    commit# 2

--- mysite/settings_base.py
+++ mysite/settings_base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
@@ -119,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ mysite/settings_staging.py
@@ -0,0 +1,6 @@
+from .settings_base import *
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
